b1dd1507e9724f35f1a9a8a4693ff8ba695128f9
Hugo Bernier <hugoabernier@live.ca>
2021-11-17 23:48:13 -0500

    Generic working version

--- src/App.tsx
+++ src/App.tsx
@@ -2,108 +2,104 @@ import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 import {
   CallComposite,
   CallAdapter,
-  createAzureCommunicationCallAdapter
+  createAzureCommunicationCallAdapter,
+  createAzureCommunicationChatAdapter,
+  ChatAdapter,
+  ChatComposite
 } from '@azure/communication-react';
-import {useAsyncMemo} from "use-async-memo"
-import { CommunicationIdentityClient }  from '@azure/communication-identity';
+import { useAsyncMemo } from "use-async-memo"
+import { CommunicationIdentityClient } from '@azure/communication-identity';
 import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 function App(): JSX.Element {
-  const queryString = window.location.search;
-  console.log(queryString);
-
   const root = document.getElementById('acs-host');
-  
-  const displayName = root?.getAttribute('data-name')+"";
-  console.log("Display Name", displayName);
+  const useChat: boolean = root?.getAttribute('data-use-chat') === 'true';
 
-  const meetingLink = root?.getAttribute('data-meetinglink')+"";
-  console.log("Meeting Link", meetingLink);
+  const displayName = root?.getAttribute('data-name') + "";
 
-  const endpointUrl = root?.getAttribute('data-endpoint')+"";
-  console.log("Endpoint URL", endpointUrl);
+  const meetingLink = root?.getAttribute('data-meetinglink') + "";
 
-  const connectionString = root?.getAttribute('data-connectionstring')+"";
-  console.log("Connection string", connectionString);
+  const connectionString = root?.getAttribute('data-connectionstring') + "";
 
   const threadId = useMemo(() => {
     // Get the threadId from the url - this also contains the call locator ID that will be removed in the threadId.split
     let threadId = meetingLink.replace('https://teams.microsoft.com/l/meetup-join/', '');
-    console.log("Thread ID Before", threadId);
+
     // Decode characters that outlook links encode
     threadId = threadId.replaceAll('%3a', ':').replace('%40', '@');
+
     // Extract just the chat guid from the link, stripping away the call locator ID
     threadId = threadId.split(/^(.*?@thread\.v2)/gm)[1];
-  
+
     if (!threadId || threadId.length === 0) throw new Error('Could not get chat thread from teams link');
-  
-    console.log("Thread ID After", threadId);
+
     return threadId;
   }, [meetingLink]);
 
+  const endpointUrl = useMemo(() => {
+    const reEndPoint: RegExp = new RegExp("endpoint=(https://.*)(?:;|$)");
+    const matchObj = reEndPoint.exec(connectionString);
+    if (matchObj && matchObj.length > 1) {
+      return matchObj[1];
+    }
+    throw new Error('Could not get endpoint from connection string');
+  }, [connectionString])
+
   //Calling Variables
   const [callAdapter, setCallAdapter] = useState<CallAdapter>();
-  
+
+  const [chatAdapter, setChatAdapter] = useState<ChatAdapter>();
+
   const tokenResponse = useAsyncMemo(async () => {
     const identityClient = new CommunicationIdentityClient(connectionString);
-    console.log("\nIdentity client", identityClient);
 
-    let identityTokenResponse = await identityClient.createUserAndToken(["voip"]);
-    const { token, expiresOn, user } = identityTokenResponse;
-    console.log(`\nCreated an identity with ID: ${user.communicationUserId}`);
-    console.log(`\nIssued an access token with 'voip' scope that expires at ${expiresOn}:`);
-    console.log(token);
-    
+    let identityTokenResponse = await identityClient.createUserAndToken(useChat ? ["voip", "chat"] : ["voip"]);
+    const { token, user } = identityTokenResponse;
+
     return {
-      token: token, 
-      userId: user.communicationUserId 
+      token: token,
+      userId: user.communicationUserId
     };
 
-  },[connectionString]);
-
-  // const credential = useMemo(() => {
-  //   try {
-  //     return new AzureCommunicationTokenCredential(tokenResponse.token);
-  //   } catch {
-  //     console.error('Failed to construct token credential');
-  //     return undefined;
-  //   }
-  // }, [tokenResponse]);
+  }, [connectionString]);
 
   useEffect(() => {
     const createAdapter = async (): Promise<void> => {
-      console.log("Create adapter", tokenResponse)
-      // setChatAdapter(
-      //   await createAzureCommunicationChatAdapter({
-      //     endpointUrl,
-      //     userId: { kind: 'communicationUser', communicationUserId: tokenResponse.userId },
-      //     displayName,
-      //     credential: new AzureCommunicationTokenCredential(tokenResponse.token),
-      //     threadId: threadId
-      //   })
-      // );
+      useChat && setChatAdapter(
+        await createAzureCommunicationChatAdapter({
+          endpointUrl,
+          userId: { kind: 'communicationUser', communicationUserId: tokenResponse.userId },
+          displayName,
+          credential: new AzureCommunicationTokenCredential(tokenResponse.token),
+          threadId: threadId
+        })
+      );
       setCallAdapter(
         await createAzureCommunicationCallAdapter({
           userId: { kind: 'communicationUser', communicationUserId: tokenResponse.userId },
           displayName,
           credential: new AzureCommunicationTokenCredential(tokenResponse.token),
-          locator: { meetingLink: meetingLink+ ""  }
+          locator: { meetingLink: meetingLink + "" }
         })
       );
     };
     tokenResponse && createAdapter();
   }, [displayName, endpointUrl, meetingLink, threadId, tokenResponse]);
 
-  if (!!callAdapter) {
+  if (!!callAdapter && !!chatAdapter) {
     return (
+      <>
         <CallComposite adapter={callAdapter} />
+        <ChatComposite adapter={chatAdapter} />
+      </>
+    );
+  } else if (!!callAdapter) {
+    return (
+      <CallComposite adapter={callAdapter} />
     );
   }
-  // if (credential === undefined) {
-  //   return <h3>Failed to construct credential. Provided token is malformed.</h3>;
-  // }
-  return <div>It works</div>;
+  return <div></div>;
 }
 
 export default App;
